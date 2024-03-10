217a2254e346ff3ce8fd7c6814a034c2e669a924
Thrisha1 <thrishakannan25@gmail.com>
2024-03-10 17:54:33 +0530

    merging

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,19 +2,18 @@ import React from 'react'
 import Round_progress from './Round_progress'
 
 function Home() {
-  return (
-    <div className = 'flex'>
-        
-        <Round_progress colour = "bg-green-500" border="border-green-500"  value="01"/>
-        <Round_progress colour = "bg-blue-500" border="border-blue-500"  value="02"/>
-        <Round_progress colour = "bg-yellow-500" border="border-yellow-500"  value="03"/>
-        <Round_progress colour = "bg-red-500" border="border-red-500"  value="08"/>
-        <Round_progress colour = "bg-purple-500" border="border-purple-500"  value="09"/>
-       
-       
-      
-    </div>
-  )
+    return (
+        <div className='flex'>
+
+            <Round_progress colour="bg-green-500" border="border-green-500" value="01"/>
+            <Round_progress colour="bg-blue-500" border="border-blue-500" value="02"/>
+            <Round_progress colour="bg-yellow-500" border="border-yellow-500" value="03"/>
+            <Round_progress colour="bg-red-500" border="border-red-500" value="08"/>
+            <Round_progress colour="bg-purple-500" border="border-purple-500" value="09"/>
+
+
+        </div>
+    )
 }
 
 export default Home
--- src/components/Round_progress.jsx
+++ src/components/Round_progress.jsx
@@ -1,34 +1,34 @@
 import React from 'react'
 
-function Round_progress({colour,value,border}) {
-  return (
-    <div>
-        <div className="w-max relative m-5">
-          <div className={`bg-white rounded-full p-1 border-2 ${border}`}>
-            <div className={`${colour} p-3 rounded-full`}>
-              <div className="bg-white rounded-full w-20 h-20 flex flex-col justify-center items-center">
-                <p className="text-black text-2xl font-bold">{value}</p>
-                <p className="text-slate-500 font-medium smaller-text">PM 2.5</p>
+function Round_progress({colour, value, border}) {
+    return (
+        <div>
+            <div className="w-max relative m-5">
+                <div className={`bg-white rounded-full p-1 border-2 ${border}`}>
+                    <div className={`${colour} p-3 rounded-full`}>
+                        <div className="bg-white rounded-full w-20 h-20 flex flex-col justify-center items-center">
+                            <p className="text-black text-2xl font-bold">{value}</p>
+                            <p className="text-slate-500 font-medium smaller-text">PM 2.5</p>
 
-              </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute top-1/2 right-[-3px]">
+                    <div className={`bg-${colour} rounded-full p-[1px] border-2 ${border}`}>
+                        <div className=" bg-white p-[2px] rounded-full">
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute top-1/2 left-[-3px]">
+                    <div className={`bg-${colour} rounded-full p-[1px] border-2 ${border}`}>
+                        <div className=" bg-white p-[2px] rounded-full">
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div className="absolute top-1/2 right-[-3px]">
-            <div className={`bg-${colour} rounded-full p-[1px] border-2 ${border}`}>
-              <div className=" bg-white p-[2px] rounded-full">
-              </div>
-            </div>
-          </div>
-          <div className="absolute top-1/2 left-[-3px]">
-          <div className={`bg-${colour} rounded-full p-[1px] border-2 ${border}`}>
-              <div className=" bg-white p-[2px] rounded-full">
-              </div>
-            </div>
-          </div>
+
         </div>
-
-    </div>
-  )
+    )
 }
 
 export default Round_progress
